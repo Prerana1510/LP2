fe6fbf66056eee918fe0aed9bb8d0a3743eec698
Prerana1510 <preranaghorpade120@gmail.com>
2026-05-05 06:05:05 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,8 @@
-num = int(input("Enter a number: "))
+# Even Odd check without user input
+
+num = 10   # yaha apna number directly likho
 
 if num % 2 == 0:
     print("Even number")
 else:
-    print("Odd number") 
+    print("Odd number")
